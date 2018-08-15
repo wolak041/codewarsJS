366154e64e32075b3e5b6fa4cca0463c8233c7da
kwolak <kwolak041@gmail.com>
2018-08-15 22:49:42 +0200

    Initial commit

---
+++ Deadfish.js
@@ -0,0 +1,16 @@
+// https://www.codewars.com/kata/51e0007c1f9378fa810002a9/solutions/javascript
+
+const parse = (data) => {
+    let value = 0;
+    const result = [];
+
+    [...data].forEach(command => {
+        if (command === 'i') value++;
+        if (command === 'd') value--;
+        if (command === 's') value *= value;
+        if (command === 'o') result.push(value);
+    });
+
+    return result;
+
+};
