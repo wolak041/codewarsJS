6cc0db7ec64773491919a90918238e19730b3575
Kamil Wolak <34286709+wolak041@users.noreply.github.com>
2019-04-18 00:14:07 +0200

    Update Deadfish.js

--- Deadfish.js
+++ Deadfish.js
@@ -1,4 +1,4 @@
-// https://www.codewars.com/kata/51e0007c1f9378fa810002a9/solutions/javascript
+// https://www.codewars.com/kata/51e0007c1f9378fa810002a9
 
 const parse = (data) => {
     let value = 0;
